f220104b718e3de313ffe044b7d3db448eebd367
Mitchell Oliver <Mitchelloliver1991@gmail.com>
2022-03-12 00:07:46 -0800

    feat: add react router and routes to elements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Newsfeed from "./pages/Newsfeed";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
     <div className="App">
-      <h1>Oliver News</h1>
+      <Router>
+        <Routes>
+          <h1>Oliver News</h1>
+          <Route path="/" element={<Newsfeed />} />
+          <Route path="/article" element={<ArticlePage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
